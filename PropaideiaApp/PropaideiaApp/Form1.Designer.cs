ed6911a898cbbe93941af3b9cd16a73732b6ce79
NikolasTol <nikolas.tolis@gmail.com>
2020-05-18 22:48:06 +0300

    Added Solution and number images

---
+++ PropaideiaApp/PropaideiaApp/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace PropaideiaApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.buttonRegister = new System.Windows.Forms.Button();
+            this.pictureBoxNo1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxNo3 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxNo6 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxNo7 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxNo10 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNo1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNo3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNo6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNo7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNo10)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(129)))), ((int)(((byte)(255)))), ((int)(((byte)(122)))));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(295, 109);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(255)))), ((int)(((byte)(125)))));
+            this.buttonLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonLogin.FlatAppearance.BorderSize = 0;
+            this.buttonLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.buttonLogin.ForeColor = System.Drawing.Color.White;
+            this.buttonLogin.Location = new System.Drawing.Point(253, 197);
+            this.buttonLogin.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(99, 37);
+            this.buttonLogin.TabIndex = 1;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = false;
+            this.buttonLogin.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonRegister
+            // 
+            this.buttonRegister.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(255)))), ((int)(((byte)(125)))));
+            this.buttonRegister.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonRegister.FlatAppearance.BorderSize = 0;
+            this.buttonRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.buttonRegister.ForeColor = System.Drawing.Color.White;
+            this.buttonRegister.Location = new System.Drawing.Point(430, 197);
+            this.buttonRegister.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonRegister.Name = "buttonRegister";
+            this.buttonRegister.Size = new System.Drawing.Size(99, 37);
+            this.buttonRegister.TabIndex = 2;
+            this.buttonRegister.Text = "Register";
+            this.buttonRegister.UseVisualStyleBackColor = false;
+            // 
+            // pictureBoxNo1
+            // 
+            this.pictureBoxNo1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxNo1.Image")));
+            this.pictureBoxNo1.Location = new System.Drawing.Point(28, 12);
+            this.pictureBoxNo1.Name = "pictureBoxNo1";
+            this.pictureBoxNo1.Size = new System.Drawing.Size(147, 175);
+            this.pictureBoxNo1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxNo1.TabIndex = 3;
+            this.pictureBoxNo1.TabStop = false;
+            // 
+            // pictureBoxNo3
+            // 
+            this.pictureBoxNo3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxNo3.Image")));
+            this.pictureBoxNo3.Location = new System.Drawing.Point(515, -1);
+            this.pictureBoxNo3.Name = "pictureBoxNo3";
+            this.pictureBoxNo3.Size = new System.Drawing.Size(147, 175);
+            this.pictureBoxNo3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxNo3.TabIndex = 4;
+            this.pictureBoxNo3.TabStop = false;
+            // 
+            // pictureBoxNo6
+            // 
+            this.pictureBoxNo6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxNo6.Image")));
+            this.pictureBoxNo6.Location = new System.Drawing.Point(93, 242);
+            this.pictureBoxNo6.Name = "pictureBoxNo6";
+            this.pictureBoxNo6.Size = new System.Drawing.Size(147, 175);
+            this.pictureBoxNo6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxNo6.TabIndex = 5;
+            this.pictureBoxNo6.TabStop = false;
+            // 
+            // pictureBoxNo7
+            // 
+            this.pictureBoxNo7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxNo7.Image")));
+            this.pictureBoxNo7.Location = new System.Drawing.Point(622, 227);
+            this.pictureBoxNo7.Name = "pictureBoxNo7";
+            this.pictureBoxNo7.Size = new System.Drawing.Size(147, 175);
+            this.pictureBoxNo7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxNo7.TabIndex = 6;
+            this.pictureBoxNo7.TabStop = false;
+            // 
+            // pictureBoxNo10
+            // 
+            this.pictureBoxNo10.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxNo10.Image")));
+            this.pictureBoxNo10.Location = new System.Drawing.Point(310, 208);
+            this.pictureBoxNo10.Name = "pictureBoxNo10";
+            this.pictureBoxNo10.Size = new System.Drawing.Size(147, 175);
+            this.pictureBoxNo10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxNo10.TabIndex = 7;
+            this.pictureBoxNo10.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(255)))), ((int)(((byte)(232)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonRegister);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBoxNo10);
+            this.Controls.Add(this.pictureBoxNo7);
+            this.Controls.Add(this.pictureBoxNo6);
+            this.Controls.Add(this.pictureBoxNo3);
+            this.Controls.Add(this.pictureBoxNo1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNo1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNo3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNo6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNo7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNo10)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Button buttonRegister;
+        private System.Windows.Forms.PictureBox pictureBoxNo1;
+        private System.Windows.Forms.PictureBox pictureBoxNo3;
+        private System.Windows.Forms.PictureBox pictureBoxNo6;
+        private System.Windows.Forms.PictureBox pictureBoxNo7;
+        private System.Windows.Forms.PictureBox pictureBoxNo10;
+    }
+}
+
