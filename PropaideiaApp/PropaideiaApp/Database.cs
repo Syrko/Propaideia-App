a7e6ef73ade3700c14c7176fd73ba3f8b083c527
Syrko <kostassir@yahoo.gr>
2020-05-20 23:46:39 +0300

    Initializing database

---
+++ PropaideiaApp/PropaideiaApp/Database.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace PropaideiaApp
+{
+    class Database
+    {
+        internal const string DATABASE_NAME = "database.db";
+        internal static void Initialize_Database()
+        {
+            using(SQLiteConnection conn = new SQLiteConnection("Data source=" + DATABASE_NAME + ";"))
+            {
+                try
+                {
+                    conn.Open();
+
+                    SQLiteCommand cmd = new SQLiteCommand(conn);
+
+                    // Create the users table
+                    cmd.CommandText = "CREATE TABLE IF NOT EXISTS users(username text NOT NULL PRIMARY KEY, " +
+                                                                        "user_type text NOT NULL," +
+                                                                        "name text NOT NULL," +
+                                                                        "surname text NOT NULL);"; // TODO change diagram
+                    cmd.Prepare();
+                    cmd.ExecuteNonQuery();
+
+                    // Create the students table
+                    cmd.CommandText = "CREATE TABLE IF NOT EXISTS students(username text NOT NULL PRIMARY KEY, " +
+                                                                            "level integer DEFAULT 1," +
+                                                                            "FOREIGN KEY(username) REFERENCES users(username));";
+                    cmd.Prepare();
+                    cmd.ExecuteNonQuery();
+
+                    // Create the professors table
+                    cmd.CommandText = "CREATE TABLE IF NOT EXISTS professors(username text NOT NULL PRIMARY KEY, " +
+                                                                            "password text NOT NULL," +
+                                                                            "FOREIGN KEY(username) REFERENCES users(username));";
+                    cmd.Prepare();
+                    cmd.ExecuteNonQuery();
+
+                    // Create the studentProgress table
+                    cmd.CommandText = "CREATE TABLE IF NOT EXISTS studentProgress(username text NOT NULL PRIMARY KEY, " +
+                                                                                    "propaideia1 integer DEFAULT 0," +
+                                                                                    "propaideia2 integer DEFAULT 0," +
+                                                                                    "propaideia3 integer DEFAULT 0," +
+                                                                                    "propaideia4 integer DEFAULT 0," +
+                                                                                    "propaideia5 integer DEFAULT 0," +
+                                                                                    "propaideia6 integer DEFAULT 0," +
+                                                                                    "propaideia7 integer DEFAULT 0," +
+                                                                                    "propaideia8 integer DEFAULT 0," +
+                                                                                    "propaideia9 integer DEFAULT 0," +
+                                                                                    "propaideia10 integer DEFAULT 0," +
+                                                                                    "finalExam integer DEFAULT 0," +
+                                                                                    "FOREIGN KEY(username) REFERENCES students(username));";
+                    cmd.Prepare();
+                    cmd.ExecuteNonQuery();
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Exception at database initialization: " + e.Message);
+                }
+            }
+        }
+
+        internal static void InsertDummyData()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection("Data source=" + DATABASE_NAME + ";"))
+            {
+                List<string> names = new List<string>()
+                {
+                    "John", "Terry", "Eric", "Michael", "Graham", "Terry",
+                };
+                List<string> surnames = new List<string>()
+                {
+                    "Cleese", "Gilliam", "Idle", "Palin", "Chapman", "Jones"
+                };
+
+                try
+                {
+                    conn.Open();
+
+                    SQLiteCommand cmd;
+
+                    // Insert users
+                    cmd = new SQLiteCommand(conn);
+                    cmd.CommandText = "INSERT INTO users(username, user_type, name, surname) " +
+                                        "VALUES(@username, @user_type, @name, @surname);";
+                    cmd.Parameters.AddWithValue("@username", surnames[0]);
+                    cmd.Parameters.AddWithValue("@user_type", UserTypes.PROFESSOR);
+                    cmd.Parameters.AddWithValue("@name", names[0]);
+                    cmd.Parameters.AddWithValue("@surname", surnames[0]);
+                    cmd.Prepare();
+                    cmd.ExecuteNonQuery();
+                    for (int i = 1; i < names.Count; i++)
+                    {
+                        cmd = new SQLiteCommand(conn);
+                        cmd.CommandText = "INSERT INTO users(username, user_type, name, surname) " +
+                                            "VALUES(@username, @user_type, @name, @surname);";
+                        cmd.Parameters.AddWithValue("@username", surnames[i]);
+                        cmd.Parameters.AddWithValue("@user_type", UserTypes.STUDENT);
+                        cmd.Parameters.AddWithValue("@name", names[i]);
+                        cmd.Parameters.AddWithValue("@surname", surnames[i]);
+                        cmd.Prepare();
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    // Insert students
+                    for (int i = 1; i < names.Count; i++)
+                    {
+                        cmd = new SQLiteCommand(conn);
+                        cmd.CommandText = "INSERT INTO students(username, level) " +
+                                            "VALUES(@username, @level);";
+                        cmd.Parameters.AddWithValue("@username", surnames[i]);
+                        cmd.Parameters.AddWithValue("@level", 1);
+                        cmd.Prepare();
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    // Insert professor
+                    cmd = new SQLiteCommand(conn);
+                    cmd.CommandText = "insert into professors(username, password) " +
+                                        "values(@username, @password);";
+                    cmd.Parameters.AddWithValue("@username", surnames[0]);
+                    cmd.Parameters.AddWithValue("@password", "admin");
+                    cmd.Prepare();
+                    cmd.ExecuteNonQuery();
+
+                    // Insert progress
+                    for (int i = 1; i < names.Count; i++)
+                    {
+                        cmd = new SQLiteCommand(conn);
+                        cmd.CommandText = "INSERT INTO studentProgress(username) " +
+                                            "VALUES(@username);";
+                        cmd.Parameters.AddWithValue("@username", surnames[i]);
+                        cmd.Prepare();
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Exception at inserting sample data: " + e.Message);
+                }
+            }
+        }
+    }
+
+    struct UserTypes
+    {
+        public const string STUDENT = "student";
+        public const string PROFESSOR = "professor";
+    }
+}
--- PropaideiaApp/PropaideiaApp/Program.cs
+++ PropaideiaApp/PropaideiaApp/Program.cs
@@ -3,20 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace PropaideiaApp
 {
     static class Program
     {
+        private const string DB_NAME = "database.db";
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            Database.Initialize_Database();
+            Database.InsertDummyData();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LoginScreen());
+        }
+
+        static void CreateTable(SQLiteConnection conn)
+        {
+
+            SQLiteCommand sqlite_cmd;
+            string Createsql = "CREATE TABLE Sample(Col1 TEXT, Col2 INTEGER)";
+            sqlite_cmd = conn.CreateCommand();
+            sqlite_cmd.CommandText = Createsql;
+            sqlite_cmd.ExecuteNonQuery();
+
         }
     }
 }
