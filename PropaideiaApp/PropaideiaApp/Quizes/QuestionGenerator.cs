f90f9ce6f9efc5c17afd9f2e5b1128b40f6af190
Syrko <kostassir@yahoo.gr>
2020-05-28 16:54:16 +0300

    Implemented QuestionGenerator

--- PropaideiaApp/PropaideiaApp/Quizes/QuestionGenerator.cs
+++ PropaideiaApp/PropaideiaApp/Quizes/QuestionGenerator.cs
@@ -8,16 +8,39 @@ namespace PropaideiaApp.Quizes
 {
 	class QuestionGenerator // TODO change diagram
 	{
-		private int quizType;
-		private int questionNum; // TODO change diagram
-		private List<>
+		private int propaideiaType;
+		private int questionNum;		// TODO change diagram
+		private List<Question> questions;
+		private int counter;
 
-		public QuestionGenerator(int quizType, int questionNum)
+		public QuestionGenerator(int propaideiaType, int questionNum)
 		{
-			this.quizType = quizType;
+			this.propaideiaType = propaideiaType;
 			this.questionNum = questionNum;
+			this.Questions = new List<Question>(questionNum);
+			this.counter = 0;
 		}
 
+		internal List<Question> Questions { get => questions; set => questions = value; }
 
+		internal void CreateNextQuestion()
+		{
+			Random rand = new Random();
+			int questionType = rand.Next(0, 3);
+
+			switch (questionType)
+			{
+				case (int)QuestionFormat.FILL_GAPS:
+					Questions[counter] = new QuestionFG(propaideiaType);
+					break;
+				case (int)QuestionFormat.TRUE_FALSE:
+					Questions[counter] = new QuestionTF(propaideiaType);
+					break;
+				case (int)QuestionFormat.MULTIPLE_CHOICE:
+					Questions[counter] = new QuestionMC(propaideiaType);
+					break;
+			}
+			counter++;
+		}
 	}
 }
