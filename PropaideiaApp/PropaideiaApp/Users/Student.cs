4ddc0ad8e0812893f7624b5718307adcf0fe8b33
Syrko <kostassir@yahoo.gr>
2020-06-21 19:21:28 +0300

    Created iverloaded constructor for registering

--- PropaideiaApp/PropaideiaApp/Users/Student.cs
+++ PropaideiaApp/PropaideiaApp/Users/Student.cs
@@ -17,6 +17,14 @@ namespace PropaideiaApp.Users
             this.studentProgress = studentProgress;
         }
 
+        /// <summary>
+        /// Overload for registering a new user. The missing fields will be defaulted by the database.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="name"></param>
+        /// <param name="surname"></param>
+        internal Student(string username, string name, string surname): base(username, name, surname) { }
+
         internal int Level { get => level; set => level = value; }
         internal StudentProgress StudentProgress { get => studentProgress; set => studentProgress = value; }
     }
